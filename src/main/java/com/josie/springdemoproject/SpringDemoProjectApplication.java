618afd1246f5edda659db2f131c6d0f7dfb65e7f
josier18 <joseline22729@gmail.com>
2023-08-26 21:38:02 -0500

    Added another print statement.

--- src/main/java/com/josie/springdemoproject/SpringDemoProjectApplication.java
+++ src/main/java/com/josie/springdemoproject/SpringDemoProjectApplication.java
@@ -10,6 +10,7 @@ public class SpringDemoProjectApplication {
 		SpringApplication.run(SpringDemoProjectApplication.class, args);
 
 		System.out.println("Hello, World");
+		System.out.println("Hello Again");
 	}
 
 }
